a56b053255bf769fc33bee70a4f25cdc938040ac
Morena96 <dowlet101@gmail.com>
2021-03-18 11:51:23 +0500

    second

--- yashyl_ada_backend/settings/_base.py
+++ yashyl_ada_backend/settings/_base.py
@@ -204,3 +204,44 @@ DEFAULT_FROM_EMAIL = get_secret('EMAIL_HOST_USER')
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://example.com'
+
+from ._base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = get_secret('DJANGO_SECRET_KEY')
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*']
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
+
+
+
+from ._base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = get_secret('DJANGO_SECRET_KEY')
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
